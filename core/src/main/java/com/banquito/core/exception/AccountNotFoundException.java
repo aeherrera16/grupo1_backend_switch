c5031da9d66c6f8b0940c3b34e882017695924f8
Anahy Herrera <aeherrera16@espe.edu.ec>
2026-05-15 21:24:18 -0500

    Limpieza de repositorio

--- core/src/main/java/com/banquito/core/exception/AccountNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.banquito.core.exception;
-
-public class AccountNotFoundException extends RuntimeException {
-
-    public AccountNotFoundException(String numeroCuenta) {
-        super("Cuenta no encontrada: " + numeroCuenta);
-    }
-}
--- core/src/main/java/com/banquito/core/exception/DuplicateTransactionException.java
+++
@@ -1,8 +0,0 @@
-package com.banquito.core.exception;
-
-public class DuplicateTransactionException extends RuntimeException {
-
-    public DuplicateTransactionException(String uuid) {
-        super("Transacción duplicada para el UUID: " + uuid);
-    }
-}
